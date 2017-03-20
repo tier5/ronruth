2b3151f92d8dde65943e5ce2d817fc1c58d42ef4
tier5 <work@tier5.us>
2017-03-20 17:18:16 +0530

    Version 2.6 Released

---
+++ wp-content/themes/ronruth/templates/learning-details.php
@@ -0,0 +1,182 @@
+<?php 
+/*
+*Template Name:Learning event details Page
+*/
+
+//get_header();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>RON RUTH</title>
+
+	<link href="<?php echo get_template_directory_uri();?>/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="<?php echo get_template_directory_uri();?>/style.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/fonts/font-awesome/css/font-awesome.min.css" type="text/css">
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" ></script>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/js/custom.js"></script>
+	<?php wp_head();?>
+	
+</head>
+<body>
+<header class="learning-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-3">
+                <div class="logo"><a href="#"><img src="<?php echo get_template_directory_uri();?>/images/logo.png" class="img-responsive" alt="Ron Ruth"></a></div>
+            </div>
+            <div class="col-md-9 col-sm-9">
+                <ul class="social">
+                    <li><a href="#"><i class="fa fa-facebook">&nbsp;</i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter">&nbsp;</i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus">&nbsp;</i></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="navigation">
+                    <ul>
+                        <li><a href="#">home</a></li>
+                        <li><a href="#">about us</a></li>
+                        <li><a href="#">events calendar</a></li>
+                        <li class="active"><a href="#">learning resources</a></li>
+                    </ul>
+                </div>
+                <div class="mobile-menu">
+                  <a href="#" class="menu-icon" style="display: block;">
+                    <span class="span bar top" style="background-color: #fff;">&nbsp;</span>
+                    <span class="span bar middle" style="background-color: #fff;">&nbsp;</span>
+                    <span class="span bar bottom" style="background-color: #fff;">&nbsp;</span>
+                  </a>
+                  <div class="main-menu">
+                    <ul>
+                        <li><a href="#">home</a></li>
+                        <li><a href="#">about us</a></li>
+                        <li><a href="#">events calendar</a></li>
+                        <li class="active"><a href="#">learning resources</a></li>
+                    </ul>
+                  </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="banner-txt text-center">
+                    <h1>Grab your <small>#</small>INSPIRAMAGINATIVITY</h1><br>
+                    <h1>on dvd!</h1>
+                    <span>(Get FREE SHIPPING Within The Continental U.S.)</span>
+                    <h2>Celebrate INSPIRAMAGINATIVITY</h2><pre>TM</pre>
+                    <p>The Playful Blending Of Inspiration, Imagination & Creativity
+To Produce Profitable Business Solutions For Wedding & DJ Professionals..</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="clear">&nbsp;</div>
+<section class="bestselling-details">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6">
+                <h3>"Ever After: Art Of Story-Selling"</h3>
+                <div class="product-img">
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                      <!-- Wrapper for slides -->
+                      <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                          <img src="<?php echo get_template_directory_uri();?>/images/book1.jpg" class="img-responsive" alt="Chania">
+                        </div>
+                        <div class="item">
+                          <img src="<?php echo get_template_directory_uri();?>/images/book2.jpg" class="img-responsive" alt="Chania">
+                        </div>
+                        <div class="item">
+                          <img src="<?php echo get_template_directory_uri();?>/images/book3.jpg" class="img-responsive" alt="Flower">
+                        </div>
+                        <div class="item">
+                          <img src="<?php echo get_template_directory_uri();?>/images/book4.jpg" class="img-responsive" alt="Flower">
+                        </div>
+                      </div>
+
+                      <!-- Left and right controls -->
+                      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                        <img src="<?php echo get_template_directory_uri();?>/images/left-arrow.png" class="img-responsive" alt="left arrow">
+                      </a>
+                      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                        <img src="<?php echo get_template_directory_uri();?>/images/right-arrow.png" class="img-responsive" alt="right arrow">
+                      </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+                <div class="product-descrp">
+                    <p>Storytelling is as old as mankind itself. Movies were once just a series of pictures etched on a cave wall. Your ability to properly convey an engaging and entertaining story will make you and your business more relatable, appealing, and persuasive. The under-utilized power of story-selling drives a bride and groom’s decision... making process.</p>
+
+                    <p>In his powerful presentation, “Ever After: The Art Of Story-Selling,” Wedding Entertainment Director®, Ron Ruth will show you how the stories you tell can give your brand an irresistible voice. You’ll learn methods for crafting memorable stories that will quickly engage prospective clients and move them to take action---plus how the story of your company’s “purpose” could well be the most important, compelling and profitable tale you’ll ever tell as a business owner to attract new customers.</p>
+                    <div class="product-price">
+                        <span class="price">$25</span>
+                        <span> &amp; free shipping</span>
+                        <p>IN THE CONTINENTAL U.S..</p>
+                        <div class="shipping">
+                            <p>FOR INTERNATIONAL SHIPPING ONLY = ADD $10.00</p>
+                            <strong>
+                                NOTE: ONLY ADD $10 ONCE FOR YOUR TOTAL DVD ORDER
+                            </strong>
+                            <p>(Purchase Additional DVDs @ U.S. Pricing)</p>
+                        </div>
+                        <div class="buy-form">
+                            <form action="">
+                                <div class="form-group">
+                                    <select name="os0" class="form-control">
+                                        <option value="Total U.S. Price Plus FREE Shipping">Total U.S. Price Plus FREE Shipping $25.00 USD </option>
+                                        <option value="Outside The U.S. Add $10.00 S&amp;H">Outside The U.S. Add $10.00 S&amp;H $35.00 USD</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" name="buynow" value="Buy now">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="clear">&nbsp;</div>
+<div class="container-fluid">
+    <div class="row"><hr></div>
+</div>
+<div class="clear">&nbsp;</div>
+<section class="review">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4>Review comments from Wedding MBA surveys:</h4>
+                <div class="each-review">
+                    <strong>Review 1</strong>
+                    <p>"Ron was fun: when he opened with Marky Mark's "Good Vibrations" video, I knew I was in for a treat. His bubbly personality and ability to engage with the audience was tremendously appreciated. He totally delivered what was advertised: storytelling. His ability to integrate his own personal experiences to his tips and suggestions for the audience was heartfelt and genuine."</p>
+                </div>
+                <div class="each-review">
+                    <strong>Review 2</strong>
+                    <p>"He was amazing. Probably my favorite class. I learned a lot."</p>
+                </div>
+                <div class="each-review">
+                    <strong>Review 3</strong>
+                    <p>"Ron Ruth got everyone going! I loved listening to him. His act on storytelling is exactly how I would like to portray my company. I want to create that story so my clients can imagine their big day with us."</p>
+                </div>
+                <div class="each-review">
+                    <strong>Review 4</strong>
+                    <p>"This guy was amazing! He had so many great tips on ways I communicate with brides. I already put them into place and have already seen results from those brides I have been communicating with!"</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="clear">&nbsp;</div>
+<?php get_footer();?>
